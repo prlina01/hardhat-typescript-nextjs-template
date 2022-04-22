6b8de93ffbbf3448218d95f399add10055837c81
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-04-22 12:39:40 +0200

    The template now uses Web3Modal instead of web3-react; Remove working index page.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {ExternalProvider, JsonRpcFetchFunc, Web3Provider} from "@ethersproject/providers";
-import {Web3ReactProvider} from "@web3-react/core";
 
-const getLibrary = (provider: ExternalProvider | JsonRpcFetchFunc) => {
-    return new Web3Provider(provider)
-}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <Web3ReactProvider getLibrary={getLibrary}>
         <Component {...pageProps} />
-      </Web3ReactProvider>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,43 @@
 import type { NextPage } from 'next'
-import {useWeb3React} from "@web3-react/core";
-import {InjectedConnector} from '@web3-react/injected-connector'
 import GreeterContract from "../artifacts/contracts/Greeter.sol/Greeter.json"
 import {ethers} from "ethers";
 import {Greeter, Greeter__factory} from "../typechain";
-import { useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {useForm} from "react-hook-form";
-import Link from 'next/link'
+import Web3Modal from "web3modal";
 
 
 
-// @ts-ignore
-const injected = new InjectedConnector()
+
 const Home: NextPage = () => {
-  const {account, deactivate, activate, active, library: provider} = useWeb3React()
   const [greetingMsg, setGreetingMsg] = useState<string>("")
-  const [hasMetamask, setHasMetamask] = useState<boolean>(false)
+  const [walletConnected, setWalletConnected] = useState(false);
+  const web3ModalRef = useRef<Web3Modal>();
 
 
-  async function connect() {
-    try {
-      await activate(injected)
-      localStorage.setItem('isWalletConnected', 'true')
-    } catch(e) {
-      console.log(e)
-    }
-  }
-
-  async function disconnect() {
-    try {
-      await deactivate()
-      localStorage.setItem('isWalletConnected', 'false')
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  useEffect(() => {
-    const connectWalletOnPageLoad = async() => {
-      if(localStorage?.getItem('isWalletConnected') === 'true') {
-        try {
-          await activate(injected)
-          localStorage.setItem('isWalletConnected', 'true')
-        } catch (e) {
-          console.log(e)
-        }
-      }
-    }
-    if(hasMetamask) connectWalletOnPageLoad()
-    // @ts-ignore
-    else if (typeof window.ethereum !== "undefined") setHasMetamask(true);
-
-  }, [])
-
-  function _getContract() {
-    const signer = provider.getSigner()
-
-
-    const contractAddress = "0x745f3C80e6c63b21b633bFc56ecB129826937Ca0"
-    const abi = GreeterContract.abi
-    const contract = Greeter__factory.connect(contractAddress, signer)
-    return contract
-  }
-
-
-  async function execute(entered_greet: { [greet: string]: string; }) {
-    if(active) {
-      const greet = entered_greet['greetInputField']
-      const contract = _getContract()
-      // const contract = new ethers.Contract(contractAddress, abi, signer)
-      try {
-        await contract.setGreeting(greet)
-        setGreetingMsg(greet)
-      } catch (e) {
-        console.log(e)
-      }
-
-    }
-  }
+  // function _getContract() {
+  //   const signer = provider.getSigner()
+  //
+  //
+  //   const contractAddress = "0x745f3C80e6c63b21b633bFc56ecB129826937Ca0"
+  //   const abi = GreeterContract.abi
+  //   const contract = Greeter__factory.connect(contractAddress, signer)
+  //   return contract
+  // }
+  // async function execute(entered_greet: { [greet: string]: string; }) {
+  //   if(active) {
+  //     const greet = entered_greet['greetInputField']
+  //     const contract = _getContract()
+  //     // const contract = new ethers.Contract(contractAddress, abi, signer)
+  //     try {
+  //       await contract.setGreeting(greet)
+  //       setGreetingMsg(greet)
+  //     } catch (e) {
+  //       console.log(e)
+  //     }
+  //
+  //   }
+  // }
 
   const {reset, register, handleSubmit, formState} = useForm()
 
@@ -87,51 +47,84 @@ const Home: NextPage = () => {
     })
   }, [reset, formState.isSubmitSuccessful])
 
-  useEffect(() => {
-    const initialGreeting = async () => {
-      const contract = _getContract()
-      const msg = await contract.greet()
-      setGreetingMsg(msg)
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = web3ModalRef.current && await web3ModalRef.current.connect();
+    const web3Provider = new ethers.providers.Web3Provider(provider);
+
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Change the network to Rinkeby");
+      throw new Error("Change network to Rinkeby");
     }
-    active && initialGreeting()
-  }, [active])
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      void connectWallet();
+    }
+  }, [walletConnected]);
+
+  const connectWallet = async () => {
+      try {
+        // Get the provider from web3Modal, which in our case is MetaMask
+        // When used for the first time, it prompts the user to connect their wallet
+        await getProviderOrSigner();
+        setWalletConnected(true);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      {hasMetamask ? (
-        active ? (
-          <>
-            <span>Connected with <b>{account}</b></span>
-            <span>Greeter: <b>{greetingMsg}</b></span>
-            <form onSubmit={handleSubmit(execute)}>
-              <label>Change greeting:</label>
-              <input {...register("greetInputField", {required: true})} className="border-2 m-2" />
-              <input type="submit" value="submit" className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white  bg-blue-600 hover:bg-blue-800"/>
-            </form>
-            <Link href="state">
-              <button className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white  bg-blue-600 hover:bg-blue-800">
-                Go to state page[metamask account stays logged in when changing pages]
-              </button>
-            </Link>
+      <h1>Ide gas</h1>
+      {/*{hasMetamask ? (*/}
+      {/*    walletConnected ? (*/}
+      {/*    <>*/}
+      {/*      <span>Connected with <b>{account}</b></span>*/}
+      {/*      <span>Greeter: <b>{greetingMsg}</b></span>*/}
+      {/*      <form onSubmit={handleSubmit(execute)}>*/}
+      {/*        <label>Change greeting:</label>*/}
+      {/*        <input {...register("greetInputField", {required: true})} className="border-2 m-2" />*/}
+      {/*        <input type="submit" value="submit" className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white  bg-blue-600 hover:bg-blue-800"/>*/}
+      {/*      </form>*/}
+      {/*      <Link href="state">*/}
+      {/*        <button className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white  bg-blue-600 hover:bg-blue-800">*/}
+      {/*          Go to state page[metamask account stays logged in when changing pages]*/}
+      {/*        </button>*/}
+      {/*      </Link>*/}
 
-            <b>Rinkeby testnet</b>
-            <button onClick={() => disconnect()} className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white bg-blue-600 hover:bg-blue-800">
-              Log out from account
-            </button>
-          </>
-      ) : (
-          <button onClick={() => connect() } className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white  bg-blue-600 hover:bg-blue-800">
-            Connect
-          </button>
-      )): (
-          <>
-            <h1>You don't have metamask installed in your browser</h1>
-            <button className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white bg-blue-600 hover:bg-blue-800">
-              <a href="https://metamask.io/download/">Install</a>
-            </button>
-          </>
-      )
-      }
+      {/*      <b>Rinkeby testnet</b>*/}
+      {/*      <button onClick={() => disconnect()} className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white bg-blue-600 hover:bg-blue-800">*/}
+      {/*        Log out from account*/}
+      {/*      </button>*/}
+      {/*    </>*/}
+      {/*) : (*/}
+      {/*    <button onClick={() => connect() } className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white  bg-blue-600 hover:bg-blue-800">*/}
+      {/*      Connect*/}
+      {/*    </button>*/}
+      {/*)): (*/}
+      {/*    <>*/}
+      {/*      <h1>You don't have metamask installed in your browser</h1>*/}
+      {/*      <button className="py-2 px-2 rounded-2xl mt-20 mb-4 text-lg font-bold text-white bg-blue-600 hover:bg-blue-800">*/}
+      {/*        <a href="https://metamask.io/download/">Install</a>*/}
+      {/*      </button>*/}
+      {/*    </>*/}
+      {/*)*/}
+      {/*}*/}
     </div>
   )
 }
